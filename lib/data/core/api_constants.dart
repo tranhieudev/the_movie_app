75372cc871b94cc01b28144bb72b26350ad3fa6b
Trần Văn Hiếu <49116308+tranhieudev@users.noreply.github.com>
2020-09-13 22:55:07 +0700

    init

---
+++ lib/data/core/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants{
+  ApiConstants._();
+
+  static const String BASE_URL ="https://api.themoviedb.org/3/";
+  static const String API_KEY ="a4e12f5bd8ee081178ac516248421253";
+  static const String BASE_IMAGE_URL="";
+}
